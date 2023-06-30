5777c72a22fa3508856aeb5371e531a2de6ebd56
WANG CHENFEI <barrycf.wang@gmail.com>
2023-06-30 13:41:01 +0800

    添加Test Option

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
 static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
 
 int main(int argc, const char *argv[]) {
+#ifdef TEST_OPTION
+  std::cout << "Test ON" << std::endl;
+#endif
   Fraction f;
 
   cout << HEADER;
